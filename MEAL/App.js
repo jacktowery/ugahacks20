750a76ff129d014e7eca3f537ec4d7eedea681c7
Jack Towery <jacktowery@gmail.com>
2020-02-08 03:47:59 -0500

    Added Stack & Tab navigation
    + Colors
    * Revised navigation
    * First draft of Byte the dog

--- MEAL/App.js
+++ MEAL/App.js
@@ -1,14 +1,15 @@
 import 'react-native-gesture-handler';
 import * as React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet, Image } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Ionicons } from 'react-native-vector-icons/Ionicons'
 
-const colorMain = '#47cc76';
-const colorSecondary = '#287543';
-const textColorMain = '#fff';
-const textColorSecondary = '#202542'
+const brandColorMain = '#3d7829';
+const brandColorDark = '#123d12';
+const brandColorLight = '#69ad45';
+const white = '#fff';
 
 function HomeScreen({navigation}) {
   return (
@@ -16,25 +17,57 @@ function HomeScreen({navigation}) {
       <Text style={styles.homePreTitle}>Presenting</Text>
       <Text style={styles.homeTitle}>MEAL</Text>
       <Text style={styles.homeSubtitle}>Making Eating Accessible Locally</Text>
-      <Text style={styles.homePreTitle}>[[IMAGE]]</Text>
+      <Image source={require('./assets/images/byte1.png')} style={styles.homeImage} />
       <Text style={styles.homeInfo}>App description goes here. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</Text>
       <Text style={styles.homeCopyright}>Copyright Jack Towery 2020</Text>
     </View>
   );
 }
 
-function LocateScreen() {
+function LocateStartScreen({navigation}) {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
       <Text>Locate Screen</Text>
+      <Button title="Submit"
+        onPress={() => {
+          navigation.navigate('LocateResults', {
+            itemId: 86,
+            param2: 'test text',
+          });
+        }}
+        />
     </View>
   );
 }
 
-function HealthScreen() {
+function LocateResultsScreen() {
   return (
     <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Health Screen</Text>
+      <Text>Locate Results Screen</Text>
+    </View>
+  );
+}
+
+function HealthStartScreen({navigation}) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Heath Screen</Text>
+      <Button title="Submit"
+        onPress={() => {
+          navigation.navigate('HealthResults', {
+            itemId: 86,
+            param2: 'test text',
+          });
+        }}
+        />
+    </View>
+  );
+}
+
+function HealthResultsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Health Results Screen</Text>
     </View>
   );
 }
@@ -64,16 +97,38 @@ function App() {
           },
         })}
         tabBarOptions={{
-          activeTintColor: colorMain,
+          activeTintColor: brandColorDark,
           inactiveTintColor: 'gray',
         }}
       >
-        <Tab.Screen name="Locate" component={LocateScreen} options={{title: 'Locate'}} />
+        <Tab.Screen name="Locate" component={LocateStackScreen} options={{title: 'Locate'}} />
         <Tab.Screen name="Home" component={HomeScreen} options={{title: 'Home',}} />
-        <Tab.Screen name="Health" component={HealthScreen} options={{title: 'Nutrition'}}/>
+        <Tab.Screen name="Health" component={HealthStackScreen} options={{title: 'Nutrition'}}/>
       </Tab.Navigator>
     </NavigationContainer>
   );
+}
+
+const LocateStack = createStackNavigator();
+
+function LocateStackScreen() {
+  return (
+    <LocateStack.Navigator>
+      <LocateStack.Screen name="LocateStart" component={LocateStartScreen} />
+      <LocateStack.Screen name="LocateResults" component={LocateResultsScreen} />
+    </LocateStack.Navigator>
+  )
+}
+
+const HealthStack = createStackNavigator();
+
+function HealthStackScreen() {
+  return (
+    <HealthStack.Navigator>
+      <HealthStack.Screen name="HealthStart" component={HealthStartScreen} />
+      <HealthStack.Screen name="HealthResults" component={HealthResultsScreen} />
+    </HealthStack.Navigator>
+  )
 }
 
 const styles = StyleSheet.create({
@@ -84,29 +139,36 @@ const styles = StyleSheet.create({
     flex: 1, 
     alignItems: 'center', 
     justifyContent: 'center',
-    backgroundColor: colorMain,
+    backgroundColor: brandColorMain,
     padding: 20
   },
   homePreTitle: {
-    color: colorSecondary
+    color: brandColorLight
   },
   homeTitle: {
-    color: textColorMain,
+    color: white,
     fontSize: 50,
-    fontWeight: 'bold'
+    fontWeight: 'bold',
+    
   },
   homeSubtitle: {
     marginTop: -10,
-    color: textColorMain,
+    color: white,
     fontSize: 20,
   },
   homeInfo: {
     marginTop: 10,
-    color: textColorSecondary
+    color: white
   },
   homeCopyright: {
     marginTop: 50,
-    color: colorSecondary
+    color: brandColorLight
+  },
+  homeImage: {
+    height: 150, 
+    width: 136,
+    marginTop: 40,
+    marginBottom: 40
   }
   
 });
